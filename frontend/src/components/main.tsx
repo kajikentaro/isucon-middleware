de9d5123d74adb1a71702383e2729d30ae13d741
kajikentaro <kajindowsxp@gmail.com>
2024-01-21 12:02:53 +0000

    make the title into link

--- frontend/src/components/main.tsx
+++ frontend/src/components/main.tsx
@@ -2,6 +2,7 @@
 import { useCurrentPageNum } from "@/hooks/use-current-page-num";
 import { useFetchRecordedTransactions } from "@/hooks/use-fetch-recorded-transactions";
 import { useFetchTotalTransactions } from "@/hooks/use-total-transactions";
+import Link from "next/link";
 import { useEffect } from "react";
 import ExecuteCheckedButton from "./execute-checked-button";
 import Pagination from "./pagination";
@@ -24,7 +25,9 @@ export default function Main() {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex w-full justify-between px-4 py-3 mb-2">
-        <h1 className="text-3xl font-bold">Isucon Middleware</h1>
+        <Link href="/">
+          <h1 className="text-3xl font-bold">Isucon Middleware</h1>
+        </Link>
         <div className="flex gap-x-5">
           <RemoveSelectedButton />
           <RemoveAllButton />
